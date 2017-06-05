7402189144076ee93874ffbd19a87ba32952165d
Tiago Prosp <tiago@prosp>
2017-06-05 18:29:17 +0100

    hero done...ish

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -12,8 +12,12 @@ const BeerHero = styled.section`
   align-items: center;
   justify-content: center;
   overflow: hidden;
-  width: 100vw;
+  width: 100%;
   height: 84.375vw;
+  background: linear-gradient(to top, rgba(9, 32, 63, 0.75) 0%, rgba(83, 120, 149, 0.75) 100%);
+  /*background-image: linear-gradient(120deg, rgba(246, 211, 101, 0.75) 0%, rgba(253, 160, 133, 0.75) 100%);*/
+  /*background-image: linear-gradient(to top, #09203f 0%, #537895 100%);*/
+  /*background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);*/
   ${media.tablet`height: 56.25vw;`}
 `
 
@@ -28,31 +32,31 @@ const BeerHeroImg = styled.img`
 
 const BeerHeader = styled.div`
   position: relative;
-  box-shadow: 0 28px 79px 0 rgba(239, 138, 52,.2);
-  margin: 2em auto;
+  margin: 0.75em auto;
+  ${media.tablet`margin: 2em auto;`}
 `
 
 const BeerHeadeline = styled.h2`
   margin: 0;
   padding: 2rem;
   text-align: center;
+  color: white;
   /*background-image: linear-gradient(to top, rgba(207, 217, 223, 0.3) 0%, rgba(226, 235, 240, 0.3) 100%);*/
-  background-image: linear-gradient(to top, rgba(253, 219, 146, 0.3) 0%, rgba(209, 253, 255, 0.4) 100%);
+  /*background: linear-gradient(to top, rgba(253, 219, 146, 0.3) 0%, rgba(209, 253, 255, 0.4) 100%);*/
 `
 
 const Hero = (props) => {
   return (
     <BeerHero>
       <BeerHeader>
-        <BeerHeadeline>DIY Beer</BeerHeadeline>
+        <BeerHeadeline>Home Made Beer</BeerHeadeline>
       </BeerHeader>
       <div>
         <BeerHeroImg src={process.env.PUBLIC_URL + 'img/hero_03.jpg'}></BeerHeroImg>
       </div>
       <div>
-        <Button>CTA</Button>
+        <Button>Beer Me</Button>
       </div>
-
     </BeerHero>
   );
 }
--- src/styled/Button.js
+++ src/styled/Button.js
@@ -1,15 +1,22 @@
 import styled from 'styled-components'
 
 const Button = styled.a`
-  background: tomato;
   display: inline-block;
+  color: white;
   cursor: pointer;
-  font-size: 1.25em;
-  padding: 1.25em 1.25em;
-  border-radius: 3px;
+  font-size: 1.35em;
+  padding: 1em 1.25em;
+  border: 2px solid white;
+  border-radius: 0.25em;
   text-align: center;
   text-decoration: none;
   vertical-align: middle;
   white-space: nowrap;
+  transition: all 0.25s ease;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.5);
+    color: #09203f;
+  }
 `
 export default Button
